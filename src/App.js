2bee0643015698f0a8f6b367863f9a4140e44f3a
Sahil9214 <utkarshsinghal369@gmail.com>
2023-02-22 21:15:58 +0530

    login page signin page button footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+// import Footer from "./Components/Footer";
+import SignIn from "./Pages/SignIn";
+import LoginPage from "./Pages/LoginPage";
+import Footer from "./Components/Footer";
 function App() {
   return (
     <div className="App">
-   
+      <br />
+<SignIn/>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATBMWzmFdFbNZAoWKULs1RkVs-CF22Yjw",
+  authDomain: "stock-collection-4e8cf.firebaseapp.com",
+  databaseURL: "https://stock-collection-4e8cf-default-rtdb.firebaseio.com",
+  projectId: "stock-collection-4e8cf",
+  storageBucket: "stock-collection-4e8cf.appspot.com",
+  messagingSenderId: "718389405388",
+  appId: "1:718389405388:web:32dc6f4a508ffa44554469"
+};
+
+
+export const app = initializeApp(firebaseConfig);
+export const auth=getAuth()

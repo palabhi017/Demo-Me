21544370857555ed4a5dd356c1b36385986fec85
RahulDudka <rahuldudka27@gmail.com>
2023-02-23 10:06:32 +0530

    homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Homepage from "./Pages/HomePage/Homepage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
--- src/Pages/HomePage/Homepage.jsx
+++ src/Pages/HomePage/Homepage.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Image } from "@chakra-ui/react";
+import styles from "../HomePage/Homepage.module.css";
+import header1 from "../../Images/header1.png";
+import header2 from "../../Images/header2.jpg";
+import header3 from "../../Images/header3.jpg";
+import header4 from "../../Images/header4.jpg";
+import header5 from "../../Images/header5.jpg";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <header className={styles.header}>
+        <Image alt="" src={header1}></Image>
+        <p>Top Categories to choose from</p>
+        <Image alt="" src={header2}></Image>
+        <Image alt="" src={header3}></Image>
+        <Image alt="" src={header4}></Image>
+        <Image alt="" src={header5}></Image>
+      </header>
+    </div>
+  );
 };
 
 export default Homepage;

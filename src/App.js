27ea2025cddedfe5f19e2224e0d577ce76b65ad0
palabhi017 <palabhi017@gmail.com>
2023-02-23 14:07:16 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-
-
 import "./App.css";
-import Navbar from "./Components/Navbar/Navbar"
-import SmallNavbar from "./Components/Navbar/SmallNavbar"
+import Navbar from "./Components/Navbar/Navbar";
+
 import AllRoutes from "./Pages/AllRoutes";
-import Footer from "./Components/Footer"
-import Card  from "./Components/Card";
-import Productpage from "./Pages/Productpage";
-
-// import Footer from "./Components/Footer";
-import SignIn from "./Pages/SignIn";
-import LoginPage from "./Pages/LoginPage";
 import Footer from "./Components/Footer";
+
 function App() {
-
-  return <div className="App">
-    <Navbar/>
-   
-    <AllRoutes/>
-    
-     <Footer/>
-  
- </div>;
-
+  return (
+    <div className="App">
+      <Navbar />
+      <AllRoutes />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/Pages/Productpage.jsx
+++ src/Pages/Productpage.jsx
@@ -1,41 +1,36 @@
-import React, { useEffect } from 'react'
-import {Box, SimpleGrid,HStack,VStack,Stack,Text} from "@chakra-ui/react"
-import {useDispatch, useSelector} from "react-redux"
-import { getProducts } from '../Redux/Products/product.action'
-import Card from "../Components/Card"
-import Filter from '../Components/Filter'
-
+import React, { useEffect } from "react";
+import { Box, SimpleGrid, HStack, VStack, Stack, Text } from "@chakra-ui/react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../Redux/Products/product.action";
+import Card from "../Components/Card";
+import Filter from "../Components/Filter";
 
 const Productpage = () => {
-   const products = useSelector((state => state.Products))
-    const dispatch =  useDispatch()
+  const products = useSelector((state) => state.Products);
+  const dispatch = useDispatch();
 
-console.log(products)
+  console.log(products);
 
-const getcate = ()=>{
-    dispatch(getProducts())
-}
-useEffect(()=>{
-    getcate()
-},[])
+  const getcate = () => {
+    dispatch(getProducts());
+  };
+  useEffect(() => {
+    getcate();
+  }, []);
   return (
     <>
-    <HStack alignItems="top">
-        <Box p="30px 10px" w="25%"  >
-         <Filter/>
+      <HStack alignItems="top">
+        <Box p="30px 10px" w="25%">
+          <Filter />
         </Box>
-        <Box pt="30px" w="75%" >
-        <SimpleGrid columns={4} pl="20px" gap="20px">
-         {products && products.map((e)=> <Card {...e}/>)}
-        </SimpleGrid>
+        <Box pt="30px" w="75%">
+          <SimpleGrid columns={4} pl="20px" gap="20px">
+            {products && products.map((e) => <Card {...e} />)}
+          </SimpleGrid>
         </Box>
-    </HStack>
-
-    
-  
+      </HStack>
     </>
-  
-  )
-}
+  );
+};
 
-export default Productpage
+export default Productpage;
